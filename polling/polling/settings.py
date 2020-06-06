8b4971e68412a02d8fda2c71f1b1c5bee37e5c04
Dimple Mathew <dimplemathew.kc@gmail.com>
2020-06-06 23:38:58 +0530

    initial changed

--- polling/polling/settings.py
+++ polling/polling/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -140,4 +139,3 @@ STATICFILES_DIRS = (
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_TMP = os.path.join(BASE_DIR, 'static')
 os.makedirs(STATIC_TMP, exist_ok=True)
-django_heroku.settings(locals())
